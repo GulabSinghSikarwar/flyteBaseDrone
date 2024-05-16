46b72f5ee336c2e699e1a7f16f3933be80fbf3f1
gulabsinghsikarwar <gulabsinghsikarwar2002@gmail.com>
2024-05-16 20:20:58 +0530

    Basic Functionality Done innially location issue solved

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
 import React, { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 import MapContainer from './components/Map/Map';
 import DialogBox from './components/utils/DialogBox';
 
 function App() {
-  const [coordinates, setCoordinates] = useState([
-    [30.67707920758048, 76.72407150381622],
-    [30.678217564150145, 76.72351811986506],
-    [30.67808449089114, 76.72363567850437]
-  ]);
-  const [isSimulating, setIsSimulating] = useState(true);
+  const [coordinates, setCoordinates] = useState([]);
+  const [newData, setNewData] = useState([]);
+  const [isSimulating, setIsSimulating] = useState(false);
   const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const [canStimulate, setCanStimulate] = useState(false); // Track whether new coordinates have been added
 
   const handleInputSubmit = (coordinatesToAdd) => {
-    // Extract latitude and longitude from each object in the array and append them to the coordinates array
-    const newCoordinates = coordinatesToAdd.map(({ latitude, longitude }) => [
-      parseFloat(latitude),
-      parseFloat(longitude)
-    ]);
-    
-    setCoordinates([...coordinates, ...newCoordinates]);
-    setIsDialogOpen(false); // Close the dialog box after submitting coordinates
+    setNewData(coordinatesToAdd);
+    setIsDialogOpen(false);
+    setCanStimulate(true); // Enable the "Simulate" button after new coordinates are added
+    toast.success('New data added! Click "Simulate" to see changes.');
   };
-  
 
   const handleSimulate = () => {
+    if (!canStimulate) {
+      console.log("here ");
+      toast.warning('Please add new data for stimulation or refresh the page.');
+      return;
+    }
     setIsSimulating(true);
-    // Simulate drone movement here
+    setCoordinates([...coordinates, ...newData.map((ele) => [ele.latitude, ele.longitude])]);
+    setNewData([]);
+    setCanStimulate(false); // Disable the "Simulate" button after simulating
+    toast.success('Click on Play Button to Play/Pause  to Perform Actions')
   };
 
   const toggleDialog = () => {
@@ -35,14 +38,15 @@ function App() {
 
   return (
     <div className="App">
+      <ToastContainer position="bottom-right" autoClose={5000} hideProgressBar={false} newestOnTop={false} closeOnClick rtl={false} pauseOnFocusLoss draggable pauseOnHover theme="dark" />
       <DialogBox isOpen={isDialogOpen} onClose={toggleDialog} onSubmit={handleInputSubmit} />
       <button onClick={toggleDialog} className="absolute top-3 right-20 z-10 px-4 py-2 bg-indigo-500 text-white rounded hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600">
         Add
       </button>
-      <button onClick={handleSimulate} className="absolute top-3 right-72 z-10 px-4 py-2 bg-indigo-500 text-white rounded hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600">
+      <button onClick={handleSimulate} className="absolute top-3 right-40 z-10 px-4 py-2 bg-indigo-500 text-white rounded hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600">
         Simulate
       </button>
-      <MapContainer coordinates={coordinates} isSimulating={isSimulating} />
+      <MapContainer coordinates={coordinates} isSimulating={isSimulating} onStimulate={handleSimulate} />
     </div>
   );
 }
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,71 +1,160 @@
-// MapContainer.js
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useRef } from 'react';
 import { Map, Marker, Polyline, GoogleApiWrapper } from 'google-maps-react';
 import { GOOGLE_API_KEY } from '../../utils/constants';
-import droneIcon from '../../assets/icons/done.png'
+import droneIcon from '../../assets/icons/done.png';
+
 const MapContainer = ({ google, coordinates, isSimulating }) => {
-  const [dronePositionIndex, setDronePositionIndex] = useState(0);
+  const [progress, setProgress] = useState([]);
+  const [lastReachedIndex, setLastReachedIndex] = useState(0); // Store the index of the last reached location
+  const velocity = 15; // Adjust velocity as needed
+  const initialDate = useRef(new Date());
+  const intervalIdRef = useRef(null);
+  const mapRef = useRef(null); // Reference to the map instance
+  const [initialCenter, setInitialCenter] = useState({ lat: 30.67707920758048, lng: 77.209 });
+  const [isPlaying, setIsPlaying] = useState(false); // Initially set to false
+  const [isPaused, setIsPaused] = useState(false);
 
   useEffect(() => {
-    if (isSimulating) {
-      // Start drone simulation
-      const intervalId = setInterval(() => {
-        setDronePositionIndex((prevIndex) => {
-          if (prevIndex < coordinates.length - 1) {
-            return prevIndex + 1;
-          } else {
-            clearInterval(intervalId);
-            return prevIndex;
-          }
-        });
-      }, 1000); // Update drone position every second
-      return () => clearInterval(intervalId);
+    if (isSimulating && coordinates.length > 0 && google && google.maps.geometry) {
+      if (!isPaused) {
+        intervalIdRef.current = setInterval(moveObject, 1000); // Update every second
+        setIsPlaying(true); // Set to true when simulation starts
+      }
+      return () => clearInterval(intervalIdRef.current);
+    } else {
+      setIsPlaying(false); // Set to false when simulation stops
     }
-  }, [coordinates, isSimulating]);
+  }, [isSimulating, coordinates, google, isPaused]); // Include coordinates, google, and isPaused as dependencies
+
+  useEffect(() => {
+    // Preserve the progress made so far when new coordinates are added
+    setProgress((prevProgress) => {
+      // Only preserve progress if there's no existing progress or the last coordinate hasn't been reached yet
+      return lastReachedIndex !== coordinates.length - 1 ? prevProgress : progress;
+    });
+    setLastReachedIndex(0); // Reset last reached index
+  }, [coordinates.length]);
+
+  const moveObject = () => {
+    if (lastReachedIndex === coordinates.length - 1) {
+      clearInterval(intervalIdRef.current); // Clear interval if the last coordinate is reached
+      setIsPlaying(false);
+      return; // Stop moving
+    }
+
+    const distance = getDistance(initialDate.current, velocity); // Calculate distance
+    const newPosition = calculatePosition(distance);
+
+    // Check if the newPosition is approximately the same as the last coordinate
+    const lastCoordinate = coordinates[coordinates.length - 1];
+    if (Math.abs(newPosition.lat - lastCoordinate[0]) < 1e-6 && Math.abs(newPosition.lng - lastCoordinate[1]) < 1e-6) {
+      clearInterval(intervalIdRef.current); // Clear interval if the last coordinate is reached
+      setIsPlaying(false);
+      return; // Stop moving
+    }
+
+    // Update progress
+    setProgress((prevProgress) => [...prevProgress, newPosition]);
+    setInitialCenter(newPosition);
+  };
+
+  const getDistance = (initialDate, velocity) => {
+    const differentInTime = (new Date() - initialDate) / 1000;
+    return differentInTime * velocity;
+  };
+
+  const calculatePosition = (distance) => {
+    if (!google || !google.maps.geometry) {
+      return { lat: coordinates[coordinates.length - 1][0], lng: coordinates[coordinates.length - 1][1] };
+    }
+
+    let accumulatedDistance = 0;
+    for (let i = lastReachedIndex; i < coordinates.length - 1; i++) {
+      const coord1 = { lat: coordinates[i][0], lng: coordinates[i][1] };
+      const coord2 = { lat: coordinates[i + 1][0], lng: coordinates[i + 1][1] };
+      const segmentDistance = google.maps.geometry.spherical.computeDistanceBetween(
+        new google.maps.LatLng(coord1),
+        new google.maps.LatLng(coord2)
+      );
+
+      if (accumulatedDistance + segmentDistance >= distance) {
+        const heading = google.maps.geometry.spherical.computeHeading(
+          new google.maps.LatLng(coord1),
+          new google.maps.LatLng(coord2)
+        );
+        const remainingDistance = distance - accumulatedDistance;
+        const position = google.maps.geometry.spherical.computeOffset(
+          new google.maps.LatLng(coord1),
+          remainingDistance,
+          heading
+        );
+        setLastReachedIndex(i); // Update lastReachedIndex
+        return { lat: position.lat(), lng: position.lng() };
+      }
+      accumulatedDistance += segmentDistance;
+    }
+
+    // If distance exceeds total route distance, return the last coordinate
+    return { lat: coordinates[coordinates.length - 1][0], lng: coordinates[coordinates.length - 1][1] };
+  };
+
+  const handlePause = () => {
+    clearInterval(intervalIdRef.current);
+    setIsPlaying(false);
+    setIsPaused(true);
+  };
+
+  const handlePlay = () => {
+    initialDate.current = new Date();
+    intervalIdRef.current = setInterval(moveObject, 1000);
+    setIsPlaying(true);
+    setIsPaused(false);
+  };
 
   return (
-    <Map google={google} zoom={18} initialCenter={{ lat: coordinates[0][0], lng: coordinates[0][1] }}>
-      {/* Markers for all coordinates except the last one */}
-      {coordinates.slice(0, -1).map((coord, index) => (
-        <Marker
-          key={index}
-          position={{ lat: coord[0], lng: coord[1] }}
-          icon={{
-            url: 'https://maps.google.com/mapfiles/ms/icons/red-dot.png',
-            scaledSize: new google.maps.Size(30, 30),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(5, 5)
-          }}
-        />
-      ))}
-      {/* Custom icon for the last coordinate */}
-      {coordinates.length > 0 && (
-        <Marker
-          position={{
-            lat: coordinates[coordinates.length - 1][0],
-            lng: coordinates[coordinates.length - 1][1]
-          }}
-          icon={{
-            url: droneIcon,
-            scaledSize: new google.maps.Size(40, 40),
-            origin: new google.maps.Point(0, 0),
-            anchor: new google.maps.Point(15, 15)
-          }}
-        />
+    <div>
+      {isSimulating && ( // Show buttons only when simulation is active
+        <div>
+          <button onClick={isPlaying ? handlePause : handlePlay} className="absolute top-20 right-40 z-10 px-4 py-2 bg-indigo-500 text-white rounded hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600"> {isPlaying ? 'Pause' : 'Play'}</button>
+
+        </div>
       )}
-      {/* Polyline */}
-      <Polyline
-        path={coordinates.map(coord => ({ lat: coord[0], lng: coord[1] }))}
-        options={{
-          strokeColor: '#FF0000',
-          strokeOpacity: 0.8,
-          strokeWeight: 2
-        }}
-      />
-    </Map>
+      <Map
+        google={google}
+        zoom={18}
+        initialCenter={{ ...initialCenter }}
+        ref={mapRef}
+        center={{ ...initialCenter }}
+      >
+        {/* Custom icon for the last coordinate */}
+        {coordinates.length > 0 && progress.length > 0 && (
+          <Marker
+            position={progress[progress.length - 1]}
+            icon={{
+              url: droneIcon,
+              scaledSize: new google.maps.Size(40, 40),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(15, 15)
+            }}
+          />
+        )}
+        {/* Polyline */}
+        {coordinates.length > 0 && progress.length > 0 && (
+          <Polyline
+            path={progress}
+            options={{
+              strokeColor: '#FF0000',
+              strokeOpacity: 0.8,
+              strokeWeight: 2
+            }}
+          />
+        )}
+      </Map>
+    </div>
   );
 };
 
 export default GoogleApiWrapper({
-  apiKey: GOOGLE_API_KEY
+  apiKey: GOOGLE_API_KEY,
+  libraries: ['geometry'] // Ensure the geometry library is loaded
 })(MapContainer);
